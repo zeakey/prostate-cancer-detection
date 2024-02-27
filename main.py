66b9acca1cd5e3d61a7222059862dbb237a7eba9
Kai ZHAO <694691@qq.com>
2024-02-27 10:32:02 -0800

    train + inference

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import torch, torchvision
 from einops import rearrange
 from dataloader import Dataloader_3D
-from models import nnUNet25D
+from models import nnUNet25D, nnUNet3D
 import argparse
 from datetime import datetime
 import numpy as np
 import os
+import pickle
 import os.path as osp
 import torch
 import torch.nn.functional as F
@@ -14,7 +15,7 @@ from torch.utils.tensorboard import SummaryWriter
 from vlkit.lrscheduler import CosineScheduler
 from vlkit import set_random_seed
 from vlkit import get_logger
-
+from vlkit.image import normalize
 
 device = torch.device('cuda:0') if torch.cuda.is_available() else torch.device('cpu')
 
@@ -25,13 +26,13 @@ def parse_args():
     parser.add_argument('--epochs',default=30,type=int,help='number of total epochs to run')
     parser.add_argument('--comment', default='', type=str)
     # parser.add_argument('--input_size',default=128,type=int,help='input size of the HW dimension of the image')
-    parser.add_argument('--lr',default=1e-2,type=float,help='learning rate')
+    parser.add_argument('--lr', default=1e-2, type=float,help='learning rate')
     parser.add_argument('--pfile_path', default='cv_ids_withNeg.p', type=str,help='relative path of each case, with grouping for cross-validation')
     parser.add_argument('--data_path',default='datasets/recentered_corrected/', type=str,help='absolute path of the whole dataset')
-    
+
     parser.add_argument('--work_dir',default='',type=str,help='dataset using')
     parser.add_argument('--workers',default=4, type=int)
-    
+
     parser.add_argument('--batch_size',default=4, type=int,help='Overall size of each batch')
     parser.add_argument('--label_set',default=2, type=float, help='0: GS=6, 1: GS>=6, 2: GS>=7, 3: TP(GS=6), 4: TP(GS>=6), 5: TP(GS>=7), 6: FN, 7: FP')
     parser.add_argument('--pe_dim',default=32, type=int,help='The dimension of vector of positional embedding')
@@ -80,22 +81,6 @@ def focal_loss(args, logits, target):
     return focal_loss
 
 
-def validate(network, dataloader, args):
-    network.eval()
-    c=0
-    loss=0
-
-    for batch, data in enumerate(dataloader):
-        img, mask = data['img'], data['mask']
-
-        img=img.to(device)
-        mask=mask.to(device)
-
-
-        logits, pred = network(img)
-        loss += focal_loss(args, logits, mask)
-    return loss / len(dataloader)
-
 
 def train(args):
     now=datetime.now()
@@ -108,10 +93,11 @@ def train(args):
     for val_idx in [0, 1, 2, 3, 4]:
 
         # Select network you want to use.
-        network=nnUNet25D(in_channels=num_channels, out_channels=1)
-        network=network.to(device)
+        network = nnUNet3D(in_channels=num_channels, out_channels=1)
+        network = network.to(device)
 
-        optimizer=torch.optim.Adam(network.parameters(), lr=args.lr)            
+        # optimizer=torch.optim.Adam(network.parameters(), lr=args.lr)
+        optimizer = torch.optim.SGD(network.parameters(), lr=args.lr, momentum=0.9, weight_decay=5e-4)
 
         train_dataset=Dataloader_3D(args, split='train', val_idx=val_idx)
         train_loader=torch.utils.data.DataLoader(
@@ -122,9 +108,9 @@ def train(args):
             pin_memory=True,
             sampler=None,
             drop_last=True)
-        val_dataloader=Dataloader_3D(args, split='val', val_idx=val_idx)
+        val_dataset=Dataloader_3D(args, split='val', val_idx=val_idx)
         val_loader=torch.utils.data.DataLoader(
-            val_dataloader,
+            val_dataset,
             batch_size=args.batch_size,
             shuffle=False,
             num_workers=args.workers,
@@ -136,7 +122,7 @@ def train(args):
             max_iters=args.epochs * len(train_loader),
             warmup_iters=5 * len(train_loader),
             max_lr=args.lr,
-            min_lr=1e-6
+            min_lr=1e-4
         )
 
         for epoch in range(args.epochs):
@@ -149,14 +135,14 @@ def train(args):
 
                 optimizer.zero_grad()
 
-                logits, pred = network(img) 
+                logits, pred = network(img)
                 loss = focal_loss(args, logits, mask)
                 loss.backward()
                 lr = lrscheduler.step()
                 for pg in optimizer.param_groups:
                     pg['lr'] = lr
                 optimizer.step()
-                
+
                 if batch % 10 == 0:
                     # find a representative image and slice
                     img_idx = mask.sum(dim=(2,3,4)).argmax().item()
@@ -175,32 +161,48 @@ def train(args):
                     red = torch.zeros_like(pz_mask)
                     green = torch.zeros_like(pz_mask)
                     blue = torch.zeros_like(pz_mask)
-                    red[:, 0].fill_(1)
+                    red[:, 2].fill_(1)
                     green[:, 1].fill_(1)
-                    blue[:, 2].fill_(1)
+                    blue[:, 0].fill_(1)
                     alpha = 0.2
-                    image[0] = (red * pz_mask + blue * tz_mask) * alpha +image[0] * (1 - alpha)
-                    image[1] = (blue * pred + green * lesion_mask) * alpha +image[1] * (1 - alpha)
-                    image[2] = (blue * pred + green * lesion_mask) * alpha +image[2] * (1 - alpha)
-                    grid = torchvision.utils.make_grid(
+                    image = torch.cat((
                         image,
-                        normalize=True,
-                    )
-                    mmcv.imwrite(grid.cpu().numpy(), osp.join(args.work_dir, 'images', f"fold{val_idx}-epoch{epoch}-iter{batch}.png"))
-                    logger.info(f"Fold [{val_idx}|5] epoch [{epoch}|{args.epochs}] iter [{batch}|{len(train_loader)}]: loss {loss.item():.3f}")
-                    global_step =  len(train_loader) * epoch + batch
+                        alpha * (red * pz_mask + blue * tz_mask) + (1 - alpha) * image,
+                        alpha * (green * lesion_mask) + (1 - alpha) * image,
+                        alpha * (blue * pred) + (1 - alpha) * image,
+                        rearrange(pred, 'h w -> 1 1 h w').repeat(3, 3, 1, 1)
+                    ))
+                    grid = torchvision.utils.make_grid(image, normalize=True, nrow=3)
+                    logger.info(f"Fold [{val_idx}|5] epoch [{epoch}|{args.epochs}] iter [{batch}|{len(train_loader)}]: lr {lr: .3e}, loss {loss.item():.3f}")
+                    global_step = len(train_loader) * epoch + batch
                     writer.add_scalar(f"fold-{val_idx}-train-loss", loss.item(), global_step=global_step)
                     writer.add_scalar(f"fold-{val_idx}-lr", lr, global_step=global_step)
-                    writer.add_image("training_samples", grid, global_step)
+                    writer.add_image(f'{val_idx}-samples', grid, global_step=global_step)
 
+                    grid = normalize(rearrange(grid, 'c h w -> h w c').cpu().numpy(), 0, 255).astype(np.uint8)
+                    mmcv.imwrite(grid, osp.join(args.work_dir, 'images', f'fold{val_idx}-epoc{epoch}-iter{batch}.png'))
+            torch.save(network.state_dict(), osp.join(args.work_dir, f'best_model_{val_idx}_final.pt'))
             # validation
             with torch.no_grad():
                 network.eval()
-                # Validation loss 
-                loss = validate(network,val_loader,args)
-                msg="Val_idx:{}, Epoch:{}, Train-Loss:{:.4f},  Val-Loss:{:.4f}".format(val_idx, epoch,loss.item(), loss)
-                logger.info(msg)
-        torch.save(network.state_dict(), osp.join(args.work_dir, f'best_model_{val_idx}_final.pt'))
+                loss = 0
+                for batch, data in enumerate(val_loader):
+                    img, mask = data['img'], data['mask']
+                    img=img.to(device)
+                    mask=mask.to(device)
+                    logits, pred = network(img)
+                    loss += focal_loss(args, logits, mask)
+
+                    if (epoch+1) % 5 == 0 or (epoch+1) == args.epochs:
+                        save_dir = osp.join(args.work_dir, 'inference', f'epoch-{epoch}')
+                        os.makedirs(save_dir, exist_ok=True)
+                        for i, path in enumerate(data['path']):
+                            study_id = path.split(os.sep)[-1]
+                            pickle.dump(pred[i, 0], open(osp.join(save_dir, study_id+"_pred_{:.4f}.p".format(0.00001)), 'wb'))
+                            pickle.dump(mask[i, 0], open(osp.join(save_dir, study_id+"_mask_{:.4f}.p".format(0.00001)), 'wb'))
+
+                loss /= len(val_loader)
+                writer.add_scalar('val-loss', loss.item())
 
 def main():
     set_random_seed(1115)    
