db3f6d726519eff8c224e5e8b957115ec01d08ae
Kai ZHAO <694691@qq.com>
2024-03-14 19:56:20 -0700

    resample

--- scripts/resample_dce.py
+++ scripts/resample_dce.py
@@ -11,7 +11,12 @@ from vlkit.io import read_dicoms
 from vlkit.image import normalize
 import vlkit.plt as vlplt
 import pickle
-from rt_utils.image_helper import get_pixel_to_patient_transformation_matrix, apply_transformation_to_3d_points
+from vlkit.dicom import (
+    get_pixel_to_patient_transformation_matrix,
+    apply_transformation_to_3d_points,
+    resample_volume,
+    read_dicoms,
+    Volume,)
 import matplotlib
 matplotlib.use('agg')
 import matplotlib.pyplot as plt
@@ -19,7 +24,7 @@ import matplotlib.pyplot as plt
 
 t2_dir = '/media/hdd2/guest/Dataset_withNeg_06082022_ForKai_03082024'
 dce_dir = '/media/hdd2/prostate-cancer-with-dce/dce-results'
-save_dir = '/media/hdd2/prostate-cancer-with-dce/dataset-with-dce'
+save_dir = '/home/kzhao/prostate-cancer-with-dce/dataset-with-dce'
 
 cases = [i for i in glob(f"{t2_dir}/*") if osp.isdir(i)]
 
@@ -27,55 +32,10 @@ miss = open('resample_missed_cases.txt', 'w')
 
 size = (320, 320)
 
-def get_voxels(dicoms):
-    mat = get_pixel_to_patient_transformation_matrix(dicoms)
-    volume = DotMap()
-    volume.intensities = torch.tensor(
-        np.concatenate(([i.pixel_array[None, :, :] for i in dicoms]), axis=0).astype(np.float32))
-
-    h, w = dicoms[0].pixel_array.shape
-    slices = len(dicoms)
-    x, y = np.meshgrid(np.arange(w), np.arange(h))
-    x = x.reshape(-1, 1)
-    y = y.reshape(-1, 1)
-
-    points = np.concatenate((x, y), axis=1)
-    coords = np.zeros((slices, h, w, 3))
-    for i in range(slices):
-        p = np.concatenate((points, np.full((len(points), 1), i)), axis=1)
-        v = apply_transformation_to_3d_points(p, mat)
-        v = v.reshape(h, w, v.shape[-1])
-        coords[i, :, :, :] = v
-    volume.coords = torch.tensor(coords).float()
-    return volume
-
-
-def resample_slice(volume0, volume1):
-    """
-    Slice-wise resample volume0 based on voxel locations of volume1
-    This assumes the slices of the two volumes are aligned in through-plane
-    """
-    if (volume0.coords[:, :, :, -1].unique().sort().values - volume1.coords[:, :, :, -1].unique().sort().values).abs().sum() > 1e-2:
-        print(volume0.coords[:, :, :, -1].unique().sort().values)
-        print(volume1.coords[:, :, :, -1].unique().sort().values)
-        raise ValueError()
-    slices = volume0.voxels.shape[-1]
-    top_left0 = volume0.coords[0, 0, 0, :2]
-    bot_right0 = volume0.coords[0, -1, -1, :2]
-    h0, w0 = (bot_right0 - top_left0).tolist()
-    normalized_coords1 = (volume1.coords[:, :, :, :2] - top_left0) / torch.tensor([h0, w0]) * 2 - 1
-    # N H W -> N 1 H W
-    data = volume0.intensities[:, None, :, :]
-    resample = DotMap()
-    resample.intensities = torch.nn.functional.grid_sample(data, normalized_coords1, align_corners=True)
-    resample.intensities = resample.intensities[:, 0, :, :]
-    resample.coords = volume1.coords
-    return resample
-
 
 for case in tqdm(cases):
     case_id = case.split(os.sep)[-1]
-    save_path = osp.join(save_dir, f"{case_id}.mat")
+    save_path = osp.join(save_dir, f"{case_id}")
     if osp.isfile(save_path):
         continue
     if not osp.isdir(osp.join(dce_dir, case_id)):
@@ -96,19 +56,19 @@ for case in tqdm(cases):
             continue
         # resample trick here
         try:
-            t2_vox = get_voxels(t2_dcm)
-            dce_vox = get_voxels(dce_dcm)
+            t2_vol = Volume(t2_dcm)
+            dce_vol = Volume(dce_dcm)
         except:
             dce_success = False
             miss.write(f"{case_id}: couldn't read voxels.\n")
             continue
-        if t2_vox.coords[:, :, :, -1].unique().size() != dce_vox.coords[:, :, :, -1].unique().size() or \
-            (t2_vox.coords[:, :, :, -1].unique().sort().values - dce_vox.coords[:, :, :, -1].unique().sort().values).abs().sum() > 1e-2:
-            if d == 'ktrans':
-                dce_success = False
-                miss.write(f"{case_id}: cannot align, bad coordinates\n")
-            continue
-        dce_resampled = resample_slice(dce_vox, t2_vox).intensities
+        # if t2_vox.coords[:, :, :, -1].unique().size() != dce_vox.coords[:, :, :, -1].unique().size() or \
+        #     (t2_vox.coords[:, :, :, -1].unique().sort().values - dce_vox.coords[:, :, :, -1].unique().sort().values).abs().sum() > 1e-2:
+        #     if d == 'ktrans':
+        #         dce_success = False
+        #         miss.write(f"{case_id}: cannot align, bad coordinates\n")
+        #     continue
+        dce_resampled = torch.tensor(resample_volume(dce_vol, t2_vol))
         if dce_resampled.shape[-2:] != size:
             dce_resampled = torch.nn.functional.interpolate(dce_resampled.unsqueeze(dim=0), size=size).squeeze()
         results[d] = dce_resampled.numpy()
@@ -133,10 +93,17 @@ for case in tqdm(cases):
     assert lesion_mask.shape[-2:] == size
     results['lesion_mask'] = lesion_mask
 
-    os.makedirs(osp.dirname(save_path), exist_ok=True)
+    os.makedirs(save_path, exist_ok=True)
     if 'ktrans' not in results:
         raise KeyError()
-    savemat(save_path, results)
+    # savemat(osp.join(save_path, "data.mat"), results)
+    #
+    t2_adc_highb = np.concatenate((results['t2'][None,], results['adc'][None,], results['highb'][None,]), axis=0)
+    dce = np.concatenate((results['ktrans'][None,], results['beta'][None,], results['kep'][None,]), axis=0)
+    np.save(osp.join(save_path, "t2-adc-highb.npy"), t2_adc_highb, allow_pickle=False)
+    np.save(osp.join(save_path, "ktrans-beta-kep.npy"), dce, allow_pickle=False)
+    np.save(osp.join(save_path, "lesion_mask.npy"), lesion_mask[None,], allow_pickle=False)
+    np.save(osp.join(save_path, "zonal_mask.npy"), zonal_mask[None,], allow_pickle=False)
 
     # visualization
     fig, axes = plt.subplots(1, len(results), figsize=(len(results)*2, 2))
@@ -155,6 +122,6 @@ for case in tqdm(cases):
         axes[i].set_title(k)
         axes[i].scatter(xs, ys, s=4, marker='x', c=colors)
     plt.tight_layout()
-    plt.savefig(save_path+'.png')
+    plt.savefig(osp.join(save_path, 'visualization.png'))
     plt.close(fig)
     miss.flush()
