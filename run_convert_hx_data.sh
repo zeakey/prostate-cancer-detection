7f8ff52f9190c2ecc345f2830e450edc7cbdb9d3
Kai ZHAO <694691@qq.com>
2024-05-17 20:10:21 -0700

    update

--- run_convert_hx_data.sh
+++ run_convert_hx_data.sh
@@ -1,23 +1,34 @@
 set -ex
 
-for method in AttentionUNet3D  nnUNet  Proposed_AtPCaNet  ResidualUNet3D  SEResUNet3D   VNet  VoxResNet #unetr
+for method in Proposed_AtPCaNet  #ResidualUNet3D  SEResUNet3D   VNet  VoxResNet unetr AttentionUNet3D  nnUNet
 do
-    evals=()
-    for seed in 0 1 2 3
+    evals_all=()
+    evals_fn=()
+    evals_patient=()
+    for seed in 0 1 2
     do
-        for scale in 0.01 0.02 0.03 0.04 0.06 0.08 0.1
+        for scale in 0.01 0.02 0.03 0.04 0.06 # 0.08 0.1
         do
-            savedir="/media/hdd2/prostate-cancer-with-dce/trick/may-17/$method/seed-$seed-randscale-$scale"
-            perpatient_path="$savedir/Proposed_AtPCaNet.perpatient.mat"
-            if [ ! -f $perpatient_path ]; then
-                    python scripts/convert_haoxin_data.py --method $method --seed $seed --save-dir $savedir --randscale $scale
-                    python evaluate.py "$savedir/$method/FN" "$savedir/$method.mat"
-                    python scripts/measurements_PerPatient.py "$savedir/$method/all" "$savedir/$method.perpatient.mat"
+            savedir="/media/hdd2/prostate-cancer-with-dce/trick/may-17b/$method/seed-$seed-randscale-$scale"
+            #
+            all="$savedir/$method.all.mat"
+            fn="$savedir/$method.falsenegative.mat"
+            patient="$savedir/$method.perpatient.mat"
+            #
+            if [[ ! -f $all || ! -f $fn || -f $patient ]]; then
+                    python scripts/convert_haoxin_data.py --method $method --seed $seed --randscale $scale --save-dir $savedir
+                    python evaluate.py "$savedir/$method/all" $all
+                    python evaluate.py "$savedir/$method/FN" $fn
+                    python scripts/measurements_PerPatient.py "$savedir/$method/all" $patient
             fi
-            evals+=( "$savedir/$method.mat" )
+            evals_all+=( $all)
+            evals_fn+=( $fn)
+            evals_patient+=($patient)
         done
     done
-    python  draw_roc.py --evals ${evals[@]} --savefig "/media/hdd2/prostate-cancer-with-dce/trick/may-17/$method-roc.pdf"
+    python  draw_roc.py --evals ${evals_all[@]} --savefig "/media/hdd2/prostate-cancer-with-dce/trick/may-17/$method-all-roc.pdf"
+    python  draw_roc.py --evals ${evals_fn[@]} --savefig "/media/hdd2/prostate-cancer-with-dce/trick/may-17/$method-fn-roc.pdf"
+    python  draw_roc.py --evals ${evals_patient[@]} --savefig "/media/hdd2/prostate-cancer-with-dce/trick/may-17/$method-patient-roc.pdf"
 done
 
 # python  draw_roc.py --evals ${evals[@]} --savefig april14-FN.pdf
