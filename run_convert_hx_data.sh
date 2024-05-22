baeb94b5558cbf4f8debba410d70f56178bbc6e0
Kai ZHAO <694691@qq.com>
2024-05-22 12:26:30 -0700

    fix ! bug

--- run_convert_hx_data.sh
+++ run_convert_hx_data.sh
@@ -1,34 +1,51 @@
 set -ex
 
+savedir="/media/hdd2/prostate-cancer-with-dce/trick/may-21test"
+
 for method in Proposed_AtPCaNet  #ResidualUNet3D  SEResUNet3D   VNet  VoxResNet unetr AttentionUNet3D  nnUNet
 do
     evals_all=()
     evals_fn=()
     evals_patient=()
+    savedir1="${savedir}/$method/origin"
+    #
+    all="$savedir1/eval.all.mat"
+    fn="$savedir1/eval.falsenegative.mat"
+    patient="$savedir1/eval.perpatient.mat"
+
+    if [[ ! -e "$all" || ! -e "$fn" || ! -e "$patient" ]]; then
+        python scripts/convert_haoxin_data.py --method $method --save-dir $savedir1
+        python evaluate.py "$savedir1/$method/all" $all
+        python evaluate.py "$savedir1/$method/FN" $fn
+        python scripts/measurements_PerPatient.py "$savedir1/$method/all" $patient
+    fi
+    evals_all+=( $all)
+    evals_fn+=( $fn)
+    evals_patient+=($patient)
     for seed in 0 1 2
     do
-        for scale in 0.01 0.02 0.03 0.04 0.06 # 0.08 0.1
+        for alpha in 2 4 8
         do
-            savedir="/media/hdd2/prostate-cancer-with-dce/trick/may-17b/$method/seed-$seed-randscale-$scale"
+            savedir1="${savedir}/$method/seed-$seed-alpha-$alpha"
             #
-            all="$savedir/$method.all.mat"
-            fn="$savedir/$method.falsenegative.mat"
-            patient="$savedir/$method.perpatient.mat"
+            all="$savedir1/eval.all.mat"
+            fn="$savedir1/eval.falsenegative.mat"
+            patient="$savedir1/eval.perpatient.mat"
             #
-            if [[ ! -f $all || ! -f $fn || -f $patient ]]; then
-                    python scripts/convert_haoxin_data.py --method $method --seed $seed --randscale $scale --save-dir $savedir
-                    python evaluate.py "$savedir/$method/all" $all
-                    python evaluate.py "$savedir/$method/FN" $fn
-                    python scripts/measurements_PerPatient.py "$savedir/$method/all" $patient
+            if [[ ! -e "$all" || ! -e "$fn" || ! -e "$patient" ]]; then
+                python scripts/convert_haoxin_data.py --method $method --rand --seed $seed --alpha $alpha --save-dir $savedir1
+                python evaluate.py "$savedir1/$method/all" $all
+                python evaluate.py "$savedir1/$method/FN" $fn
+                python scripts/measurements_PerPatient.py "$savedir1/$method/all" $patient
             fi
             evals_all+=( $all)
             evals_fn+=( $fn)
             evals_patient+=($patient)
         done
     done
-    python  draw_roc.py --evals ${evals_all[@]} --savefig "/media/hdd2/prostate-cancer-with-dce/trick/may-17/$method-all-roc.pdf"
-    python  draw_roc.py --evals ${evals_fn[@]} --savefig "/media/hdd2/prostate-cancer-with-dce/trick/may-17/$method-fn-roc.pdf"
-    python  draw_roc.py --evals ${evals_patient[@]} --savefig "/media/hdd2/prostate-cancer-with-dce/trick/may-17/$method-patient-roc.pdf"
+    python  draw_roc.py --evals ${evals_all[@]} --savefig "$savedir/$method-all-roc.pdf"
+    python  draw_roc.py --evals ${evals_fn[@]} --savefig "$savedir/$method-fn-roc.pdf"
+    python  draw_roc.py --evals ${evals_patient[@]} --savefig "$savedir/$method-patient-roc.pdf"
 done
 
 # python  draw_roc.py --evals ${evals[@]} --savefig april14-FN.pdf
