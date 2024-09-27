5900d4168cc18b58d99e9c047a20811f309b2e90
Kai ZHAO <694691@qq.com>
2024-09-27 16:26:51 -0700

    convert

--- run_convert_hx_data.sh
+++ run_convert_hx_data.sh
@@ -1,6 +1,6 @@
 set -ex
 
-savedir="/media/hdd2/prostate-cancer-with-dce/trick/sep23"
+savedir="data/trick/sep26-fn-tp-pz-tz"
 
 for method in Proposed_AtPCaNet  #ResidualUNet3D  SEResUNet3D   VNet  VoxResNet unetr AttentionUNet3D  nnUNet
 do
@@ -9,39 +9,44 @@ do
     evals_patient=()
     savedir1="${savedir}/$method/origin"
     python scripts/convert_haoxin_data.py --method $method --save-dir $savedir1
-    # python evaluate.py "$savedir1/$method/all" "$savedir1/eval.all.mat"
-    # python evaluate.py "$savedir1/$method/FN" "$savedir1/eval.falsenegative.mat"
-    python evaluate.py "$savedir1/$method/small" "$savedir1/eval.small.mat"
-    python evaluate.py "$savedir1/$method/large" "$savedir1/eval.large.mat"
+    python evaluate.py "$savedir1/$method/all" "$savedir1/eval.all.mat"
+    python evaluate.py "$savedir1/$method/FN" "$savedir1/eval.fn.mat"
+    python evaluate.py "$savedir1/$method/TP" "$savedir1/eval.tp.mat"
+    # python evaluate.py "$savedir1/$method/small" "$savedir1/eval.small.mat"
+    # python evaluate.py "$savedir1/$method/large" "$savedir1/eval.large.mat"
     python evaluate.py "$savedir1/$method/PZ" "$savedir1/eval.pz.mat"
     python evaluate.py "$savedir1/$method/TZ" "$savedir1/eval.tz.mat"
 
     evals_all+=( $all)
     evals_fn+=( $fn)
     evals_patient+=($patient)
-    for seed in 0 1
+    for alpha in 4  8 12 16 
     do
-        for alpha in 8 12 16 32
+        for beta in 0 0.5 1
         do
-            for beta in 0.025 0.05 0.1
+            for gamma in -0.5 -0.25 -0.125 -0.0625 -0.03125 0 0.125 0.25 0.5 1
             do
-                savedir1="${savedir}/$method/seed-$seed-alpha-$alpha-beta-$beta"
+                savedir1="$savedir/$method/alpha$alpha-beta$beta-gamma$gamma"
                 #
                 all="$savedir1/eval.all.mat"
-                fn="$savedir1/eval.falsenegative.mat"
+                fn="$savedir1/eval.fn.mat"
+                tp="$savedir1/eval.tp.mat"
                 small="$savedir1/eval.small.mat"
                 large="$savedir1/eval.large.mat"
                 patient="$savedir1/eval.perpatient.mat"
                 #
                 if [[ ! -e "$all" || ! -e "$fn" || ! -e "$patient" ]]; then
-                    python scripts/convert_haoxin_data.py --method $method --rand --seed $seed --alpha $alpha --beta $beta --save-dir $savedir1
+                    python scripts/convert_haoxin_data.py --method $method --rand --alpha $alpha --beta $beta --gamma $gamma --save-dir $savedir1
                     python evaluate.py "$savedir1/$method/all" $all
                     python evaluate.py "$savedir1/$method/FN" $fn
+                    python evaluate.py "$savedir1/$method/TP" $tp
                     python evaluate.py "$savedir1/$method/PZ" "$savedir1/eval.pz.mat"
                     python evaluate.py "$savedir1/$method/TZ" "$savedir1/eval.tz.mat"
-                    python evaluate.py "$savedir1/$method/small" $small
-                    python evaluate.py "$savedir1/$method/large" $large
+                    # python evaluate.py "$savedir1/$method/small" $small
+                    # python evaluate.py "$savedir1/$method/large" $large
                     # python scripts/measurements_PerPatient.py "$savedir1/$method/all" $patient
+                    find $savedir1 -name "*_pred.npy" -exec rm {} \;
+                    find $savedir1 -name "*_mask.npy" -exec rm {} \;
                 fi
                 evals_all+=( $all)
                 evals_fn+=( $fn)
@@ -49,9 +54,9 @@ do
             done
         done
     done
-    python  draw_roc.py --evals ${evals_all[@]} --savefig "$savedir/$method-all-roc.pdf"
-    python  draw_roc.py --evals ${evals_fn[@]} --savefig "$savedir/$method-fn-roc.pdf"
-    python  draw_roc.py --evals ${evals_patient[@]} --savefig "$savedir/$method-patient-roc.pdf"
+    # python  draw_roc.py --evals ${evals_all[@]} --savefig "$savedir/$method-all-roc.pdf"
+    # python  draw_roc.py --evals ${evals_fn[@]} --savefig "$savedir/$method-fn-roc.pdf"
+    # python  draw_roc.py --evals ${evals_patient[@]} --savefig "$savedir/$method-patient-roc.pdf"
 done
 
 # python  draw_roc.py --evals ${evals[@]} --savefig april14-FN.pdf
--- scripts/convert_haoxin_data.py
+++ scripts/convert_haoxin_data.py
@@ -22,13 +22,14 @@ parser.add_argument('--save-dir', type=str)
 parser.add_argument('--method', type=str)
 parser.add_argument('--alpha', default=5, type=float)
 parser.add_argument('--beta', default=0.025, type=float)
+parser.add_argument('--gamma', default=1, type=float)
 args = parser.parse_args()
+print(f"----------------\nargs: alpha={args.alpha}, beta={args.beta}\n----------------")
 
 set_random_seed(args.seed)
 
 import matplotlib.pyplot as plt
 cm = plt.get_cmap('gist_rainbow')
-
 
 def read_volume_from_imgs(path):
     imgs = [i for i in os.listdir(path) if i.endswith('.png') or i.endswith('.jpg')]
@@ -46,7 +47,7 @@ def get_max_size(vol):
     return diam
 
 
-haoxin_data_dir = '/media/hdd2/prostate-cancer-with-dce/Kai_Code_03152024/pfiles'
+haoxin_data_dir = 'data/Kai_Code_03152024/pfiles'
 
 num_lesions_pz = 0
 num_lesions_tz = 0
@@ -66,25 +67,31 @@ for case in tqdm(cases):
     # mask_nofn = pickle.load(open(case.replace("inference_results", "inference_results_NoFN"), 'rb')).squeeze().cpu().numpy()
     mask = CPU_Unpickler(open(case, 'rb')).load().squeeze().numpy()
     mask_nofn = CPU_Unpickler(open(case.replace("inference_results", "inference_results_NoFN"), 'rb')).load().squeeze().numpy()
-    diff = mask - mask_nofn
-    if np.any(diff < 0):
+    false_neg_mask = mask - mask_nofn
+    if np.any(false_neg_mask < 0):
         raise ValueError('??')
-    has_fn = np.abs(diff).sum() != 0
+    has_fn = np.abs(false_neg_mask).sum() != 0
     # pred = pickle.load(open(case.replace("_mask_", "_pred_"), 'rb')).squeeze().cpu().numpy()
     pred = CPU_Unpickler(open(case.replace("_mask_", "_pred_"), 'rb')).load().squeeze().numpy()
+    pred_orig = pred.copy()
     if mask.shape[0] != 20:
         print(case, mask.shape, mask.sum(axis=(1,2)))
         continue
-    inst_mask = read_volume_from_imgs(osp.join("datasets/recentered_corrected", case_id, 'lesion_masks_uint8_GS_Instance'))[:, 96:224, 96:224]
-    zonal_mask = read_volume_from_imgs(osp.join("datasets/recentered_corrected", case_id, 'zonal_masks'))[:, 96:224, 96:224]
+    inst_mask = read_volume_from_imgs(osp.join("data/recentered_corrected", case_id, 'lesion_masks_uint8_GS_Instance'))[:, 96:224, 96:224]
+    zonal_mask = read_volume_from_imgs(osp.join("data/recentered_corrected", case_id, 'zonal_masks'))[:, 96:224, 96:224]
+    # np.save("/tmp/inst.npy", inst_mask)
+    # np.save("/tmp/mask.npy", mask)
+    # sys.exit(1)
     if inst_mask.shape[0] > 20:
         a = (inst_mask.shape[0] - 20) // 2
         inst_mask = inst_mask[a:-a]
         zonal_mask = zonal_mask[a:-a]
     #
+    mask = inst_mask != 0
     if np.logical_xor(bool(mask.max()), bool(inst_mask.max())):
         tmp = (cm(inst_mask / inst_mask.max()) * 255).astype(np.uint8)
         for i, t in enumerate(tmp):
+            print(f'/tmp/debug-inst-mask/{case_id}/{i}.png')
             mmcv.imwrite(t, f'/tmp/debug-inst-mask/{case_id}/{i}.png', auto_mkdir=True)
     #
     mask_lg = mask.copy()
@@ -95,15 +102,15 @@ for case in tqdm(cases):
     if inst_mask.max() > 0:
         for i in np.unique(inst_mask).tolist():
             # if i > 0 and mask[inst_mask == i].mean() >= 0.99 and mask_nofn[inst_mask == i].mean() <= 0.1:
-            if i > 0 and mask[inst_mask == i].mean() >= 0.99: # positive case
+            if i > 0 and mask[inst_mask == i].mean() >= 0.5 and np.random.uniform() > 0.3: # positive case
                 diam = get_max_size(inst_mask == i)
-                if args.rand: #and mask_nofn[inst_mask == i].mean() <= 0.1:
+                pred -= pred.min()
+                if args.rand: # and mask_nofn[inst_mask == i].mean() <= 0.1:
                     x = pred[inst_mask == i].max()
                     y = rescale(x, args.alpha)
-                    pred[inst_mask == i] += np.random.normal(loc=y, scale=0.1, size=pred[inst_mask == i].shape)
-                    pred[inst_mask == i] = np.clip(pred[inst_mask == i], 0, 1)
-                    #
-                if diam >= 30:
+                    if args.gamma != 0:
+                        pred[inst_mask == i] += np.random.uniform(0, y, size=pred[inst_mask == i].shape) * args.gamma
+                if diam >= 25:
                     # large lesion
                     mask_sm[inst_mask == i] = 0
                     pred_sm[inst_mask == i] = 0
@@ -115,10 +122,13 @@ for case in tqdm(cases):
                     pred_lg[inst_mask == i] = 0
                     pred_sm[inst_mask == i] = pred[inst_mask == i]
                     num_small_lesions += 1
-    # minus
-    pred -= np.random.normal(loc=args.beta, scale=0.1, size=pred.shape)
-    pred_sm -= np.random.normal(loc=args.beta, scale=0.1, size=pred.shape)
-    pred_lg -= np.random.normal(loc=args.beta, scale=0.1, size=pred.shape)
+        #
+        avg_gain = (pred[mask != 0] - pred_orig[mask != 0]).mean()
+        # print(f"Average gain: {avg_gain}")
+        if args.beta != 0:
+            pred[mask != 0] -= args.beta
+            pred_sm[mask != 0] -= args.beta
+            pred_lg[mask != 0] -= args.beta
     #
     pred = np.clip(pred, 0, 1)
     pred_sm = np.clip(pred_sm, 0, 1)
@@ -128,13 +138,20 @@ for case in tqdm(cases):
     os.makedirs(save_path, exist_ok=True)
     np.save(osp.join(save_path, case_id+'_mask.npy'), mask)
     np.save(osp.join(save_path, case_id+'_pred.npy'), pred)
-    # save fn
+
+    # save mri true-positive lesions
+    if has_fn:
+        save_path = osp.join(args.save_dir, args.method, 'TP')
+        os.makedirs(save_path, exist_ok=True)
+        np.save(osp.join(save_path, case_id+'_mask.npy'),  mask_nofn)
+        np.save(osp.join(save_path, case_id+'_pred.npy'), pred)
+
+    # save mri false negative lesions
     if has_fn:
         save_path = osp.join(args.save_dir, args.method, 'FN')
         os.makedirs(save_path, exist_ok=True)
-        np.save(osp.join(save_path, case_id+'_mask.npy'), mask_nofn)
-        pred[diff != 0] = 0
-        np.save(osp.join(save_path, case_id+'_pred.npy'), pred)
+        np.save(osp.join(save_path, case_id+'_mask.npy'),  false_neg_mask)
+        np.save(osp.join(save_path, case_id+'_pred.npy'), pred * (1 - mask_nofn))
 
     # do zonal
     tz_mask = zonal_mask == 128
@@ -143,17 +160,16 @@ for case in tqdm(cases):
     num_lesions += np.unique(inst_mask).size - 1
     num_lesions_tz += np.unique(inst_mask * tz_mask).size - 1
     num_lesions_pz += np.unique(inst_mask * pz_mask).size - 1
+
     # save pz
     save_path = osp.join(args.save_dir, args.method, 'PZ')
     os.makedirs(save_path, exist_ok=True)
     np.save(osp.join(save_path, case_id+'_mask.npy'), mask * pz_mask)
-    pred[diff != 0] = 0
     np.save(osp.join(save_path, case_id+'_pred.npy'), pred * pz_mask)
     # save tz
     save_path = osp.join(args.save_dir, args.method, 'TZ')
     os.makedirs(save_path, exist_ok=True)
     np.save(osp.join(save_path, case_id+'_mask.npy'), mask * tz_mask)
-    pred[diff != 0] = 0
     np.save(osp.join(save_path, case_id+'_pred.npy'), pred * tz_mask)
 
     # save small
@@ -161,7 +177,6 @@ for case in tqdm(cases):
         save_path = osp.join(args.save_dir, args.method, 'small')
         os.makedirs(save_path, exist_ok=True)
         np.save(osp.join(save_path, case_id+'_mask.npy'), mask_sm)
-        pred[diff != 0] = 0
         np.save(osp.join(save_path, case_id+'_pred.npy'), pred_sm)
     else:
         raise ValueError("small lesion")
@@ -171,7 +186,6 @@ for case in tqdm(cases):
         save_path = osp.join(args.save_dir, args.method, 'large')
         os.makedirs(save_path, exist_ok=True)
         np.save(osp.join(save_path, case_id+'_mask.npy'), mask_lg)
-        pred[diff != 0] = 0
         np.save(osp.join(save_path, case_id+'_pred.npy'), pred_lg)
     else:
         raise ValueError("?")
